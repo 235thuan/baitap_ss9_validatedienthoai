5ca4ff40166d16c7dc34e082cdea1a2e0f4eb9b4
Thuan Nguyen <235thuan@gmail.com>
2019-05-21 17:04:36 +0700

    nop lan 1

---
+++ dienthoai.js
@@ -0,0 +1,8 @@
+let name="(84)-(0978489648)";
+let name1="@gmail.com";
+let name2="ab@gmail.";
+let name3="@#abc@gmail.com";
+let pattern=/^\([0-9]{2,}\)\-\([0-9]+\)$/;
+let result=pattern.test(name);
+alert(result);
+
